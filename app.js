d167cd6e9e46be6b2e2333f04491ab7e7002e7c8
vnling <veenling@gmail.com>
2021-10-27 23:56:53 -0400

    basic registration and login functionality

---
+++ app.js
@@ -0,0 +1,92 @@
+var express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    User = require("./models/user");
+
+mongoose.connect("mongodb://localhost/auth_demo_app");
+ 
+var app = express();
+app.set("view engine", "html");
+app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+app.use(require("express-session")({
+    secret: "Rusty is a dog",
+    resave: false,
+    saveUninitialized: false
+}));
+ 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+ 
+//=====================
+// ROUTES
+//=====================
+ 
+// Showing login page
+app.get("/", function (req, res) {
+    res.render("index");
+});
+ 
+// Showing home page
+app.get("/homepage", isLoggedIn, function (req, res) {
+    res.render("homepage");
+});
+ 
+// Showing register form
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+ 
+// Handling user signup
+app.post("/register", function (req, res) {
+    var username = req.body.username
+    var password = req.body.password
+    User.register(new User({ username: username }),
+            password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+ 
+        passport.authenticate("local")(
+            req, res, function () {
+            res.render("homepage");
+        });
+    });
+});
+ 
+//Showing login form
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+ 
+//Handling user login
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/homepage",
+    failureRedirect: "/login"
+}), function (req, res) {
+});
+ 
+//Handling user logout
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
+ 
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/login");
+}
+ 
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Server Has Started!");
+});
