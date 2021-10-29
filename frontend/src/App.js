d8ac4276796b40179c01941405b91e13a5755c4b
dleguisamo <david.leguisamo@nyu.edu>
2021-10-29 14:21:46 -0400

    Adding component files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css"; //install bootstrap dependency "npm install bootstrap"
 
+//import components
+import Homepage from "./components/homepage";
+import Login from "./components/login";
+//ENTRY POINT FOR APPLICATION
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello world.
+
     </div>
   );
 }
---
+++ frontend/src/components/homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Homepage() {
+    return (
+      <div className="App">
+        Hello world.
+  
+      </div>
+    );
+  }
+
+  export default Homepage;
