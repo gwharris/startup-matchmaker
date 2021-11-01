48187aa5b99f27ff54659400d18c05f31a1f18bd
dleguisamo <david.leguisamo@nyu.edu>
2021-10-31 23:12:05 -0400

    cleanup + more experimenting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,13 @@ import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"; //install bootstrap dependency "npm install bootstrap"
 
 //import components
-import Homepage from "./components/homepage";
+import homepage from "./components/homepage";
 import Login from "./components/login";
 //ENTRY POINT FOR APPLICATION
 function App() {
   return (
     <div className="App">
-      Hello world.
-
+      <h1>Hello From React!</h1>
     </div>
   );
 }
