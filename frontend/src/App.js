657276bb5f257cb62fed38a2ef19fde9efed3e7e
dleguisamo <david.leguisamo@nyu.edu>
2021-11-01 23:55:12 -0400

    Playing around with bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,19 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"; //install bootstrap dependency "npm install bootstrap"
 
+import {Button} from 'react-bootstrap';
+
 //import components
-import homepage from "./components/homepage";
-import Login from "./components/login";
+//import _ from "./components/__";
+
 //ENTRY POINT FOR APPLICATION
 function App() {
   return (
     <div className="App">
-      <h1>Hello From React!</h1>
+      <header className="App-header">
+        <Button>Test</Button>
+      </header>
+
     </div>
   );
 }
