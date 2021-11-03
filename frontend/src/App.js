4b9c5fa4f3bfa789a62294e7e3854e0713e8b170
dleguisamo <david.leguisamo@nyu.edu>
2021-11-02 23:53:11 -0400

    Creation and styling of homepage component. (partial)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+//import { Switch, Route, Link } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css"; //install bootstrap dependency "npm install bootstrap"
 import './index.css';
 
-import {Button} from 'react-bootstrap';
-
 //import components
-//import _ from "./components/__";
+import { HomePage } from "./components";
 
 //ENTRY POINT FOR APPLICATION
 function App() {
   return (
-    <div className="App">
-
-      <header className="App-header">
-        <Button className="button">Test</Button>
-      </header>
-
-    </div>
+    //homepage component
+    <HomePage></HomePage>
   );
 }
 
---
+++ frontend/src/components/index.js
@@ -0,0 +1,4 @@
+//import any components made here, and export them as well. This will make importing them in App.js much easier
+import HomePage from './homepage'
+
+export { HomePage }
