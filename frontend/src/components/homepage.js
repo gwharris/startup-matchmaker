1b3cb1b402ceb21b265b413dafbc03217723216c
dleguisamo <david.leguisamo@nyu.edu>
2021-11-03 14:11:35 -0400

    Finished homepage + implemented elementary event handling

--- frontend/src/components/homepage.js
+++ frontend/src/components/homepage.js
@@ -2,14 +2,25 @@ import React from 'react'
 import {Button, Container, Row, Col} from 'react-bootstrap';
 
 const HomePage = (props) => {
+
+    const handleStartupClick = () => {
+        console.log("SELECTED STARTUP")
+    }
+
+    const handlePersonClick = () => {
+        console.log("SELECTED PERSON")
+    }
+    
     return (
         <Container fluid className='hpContainer'>
             <Row className='hpRow'>
                 <Col className="leftCol">
-                    <Button className="startupButton">STARTUP?</Button>
+                    <div className='startupQuestion'>ARE YOU A</div>
+                    <Button onClick={handleStartupClick} className="startupButton">STARTUP?</Button>
                 </Col>
                 <Col className='rightCol'>
-                    <Button className='personButton'>PERSON?</Button>
+                    <div className='personQuestion'>ARE YOU A</div>
+                    <Button onClick={handlePersonClick} className='personButton'>PERSON?</Button>
                 </Col>
             </Row>
         </Container>
