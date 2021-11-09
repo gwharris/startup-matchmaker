1265bbec20d84f0ad06bd14347e13ba0beb75905
dleguisamo <david.leguisamo@nyu.edu>
2021-11-08 23:00:03 -0500

    Completed Registration page

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import './styles/register.css';
+import {Button, Container, Row, Col, Form, FloatingLabel} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h2>This is the registration page</h2>
-        </div>
+        <Container className='rgContainer'>
+            <Row className='row'>
+                <Col className='midCol'>
+                    <div className='register'>Register</div>
+                    <Form>
+
+                        <Form.Group className="nameForm">
+                            <FloatingLabel label="full name">
+                                <Form.Control className="nameBorder" type="name" placeholder="full name" />
+                            </FloatingLabel>
+                        </Form.Group>
+
+                        <Form.Group className = 'rpEmailForm'>
+                            <FloatingLabel label="email">
+                                <Form.Control className="rpEmailBorder" type="email" placeholder="email" />
+                            </FloatingLabel>
+                        </Form.Group>
+
+                        <Form.Group className="rpPasswordForm">
+                            <FloatingLabel label="password">
+                                <Form.Control className="rpPasswordBorder" type="password" placeholder="password" />
+                            </FloatingLabel>
+                        </Form.Group>
+
+                        <Link to="/login">
+                            <Button className='createAcctButton'>Create Account</Button>
+                        </Link>
+                        
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+
     )
 }
 
