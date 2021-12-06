8dcb564eddb7d6ed603c5ff35730f39c7e6f5118
dleguisamo <david.leguisamo@nyu.edu>
2021-12-06 13:46:02 -0500

    Cleaning and styling "edit profile" page for person. Changes to fields are saved to state

--- frontend/src/components/EditPersonProfile.js
+++ frontend/src/components/EditPersonProfile.js
@@ -1,28 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import './styles/editPersonProfile.css';
 import { Button, Container, Row, Col, Form} from 'react-bootstrap';
-import axios from 'axios'
+import axios from 'axios';
+import Select from 'react-select'
 
 const EditPersonProfile = () => {
 
     // Skills to be used eventually
-    // const personSkills = [
-    //     { value: 'software engineering', label:'Software Engineering'},
-    //     { value: 'marketing', label:'Marketing'},
-    //     { value: 'product development', label:'Product Development'},
-    //     { value: 'consulting', label:'Consulting'},
-    //     { value: 'graphic design', label:'Graphic Design'},
-    //     { value: 'design', label:'Design'},
-    //     { value: 'growth hacking', label:'Growth Hacking'},
-    //     { value: 'social media', label:'Social Media'},
-    //     { value: 'data', label:'Data'},
-    //     { value: 'hardware engineering', label:'Hardware Engineering'},
-    //     { value: 'biology', label:'Biology'},
-    //     { value: 'chemistry', label:'Chemistry'},
-    //     { value: 'physics', label:'Physics'},
-    //     { value: 'research', label:'Research'}
+    const personSkills = [
+        { value: 'software engineering', label:'Software Engineering'},
+        { value: 'marketing', label:'Marketing'},
+        { value: 'product development', label:'Product Development'},
+        { value: 'consulting', label:'Consulting'},
+        { value: 'graphic design', label:'Graphic Design'},
+        { value: 'design', label:'Design'},
+        { value: 'growth hacking', label:'Growth Hacking'},
+        { value: 'social media', label:'Social Media'},
+        { value: 'data', label:'Data'},
+        { value: 'hardware engineering', label:'Hardware Engineering'},
+        { value: 'biology', label:'Biology'},
+        { value: 'chemistry', label:'Chemistry'},
+        { value: 'physics', label:'Physics'},
+        { value: 'research', label:'Research'}
     
-    // ];
+    ];
 
     //states to be populated
     const [ name, setName ] = useState(null);
@@ -30,7 +31,7 @@ const EditPersonProfile = () => {
     const [ title, setTitle ] = useState(null);
     const [ bio, setBio ] = useState(null);
     const [ contact, setContact ] = useState(null);
-    const [ skills, setSkill ] = useState(null);
+    const [ skills, setSkills ] = useState(null);
 
     //on mount, data for the user that is logged in should be visible in editable forms
     const [error, setError] = useState(null);
@@ -60,44 +61,77 @@ const EditPersonProfile = () => {
     if (loading) return "Loading data...";
     if (error) return "An error!"
 
+    //Custom Styles for Skills Dropdown Menu
+    // const customStyles = {
+    //     control: base => ({
+    //         ...base,
+    //         border: 0,
+    //         // This line disable the blue border
+    //         boxShadow: 'none'
+    //       })
+    //   }
+
+    // Method to test that skills are being stored as array in state....it works!
+    const consoleLogSkills = () => {
+        console.log(skills);
+    }
 
     return (
-        <Container>
-            <Row>
-                <Col>
-                <p className='pEditProfileTitle'>Edit Profile</p>
+        <Container className='pEditProfileContainer'>
+            <Row className='pEditProfileRow'>
+                <Col className='pEditProfileCol'>
+
+                    <p className='pEditProfileTitle'>Edit Profile</p>
                     <Form>
 
                         <Form.Group className='pEditNameForm'>
                             <Form.Label>Name</Form.Label>
-                            <Form.Control value={name} onChange={e=> setName(e.target.value)} type='name'/>
+                            <Form.Control className='pEditNameBorder' value={name} onChange={e=> setName(e.target.value)} type='name'/>
                         </Form.Group>
 
                         
                         <Form.Group className='pEditTitleForm'>
                             <Form.Label>Title</Form.Label>
-                            <Form.Control value={title} onChange={e=> setTitle(e.target.value)} type='title'/>
+                            <Form.Control className='pEditTitleBorder' value={title} onChange={e=> setTitle(e.target.value)} type='title'/>
                         </Form.Group>
 
                         
                         <Form.Group className='pEditOrgForm'>
                             <Form.Label>Organization</Form.Label>
-                            <Form.Control value={organization} onChange={e=> setOrganization(e.target.value)} type='org'/>
+                            <Form.Control className='pEditOrgBorder' value={organization} onChange={e=> setOrganization(e.target.value)} type='org'/>
                         </Form.Group>
                         
 
                         <Form.Group className='pEditBioForm'>
                             <Form.Label>Bio</Form.Label>
-                            <Form.Control value={bio} onChange={e=> setBio(e.target.value)} type='bio' as='textarea'/>
+                            <Form.Control className='pEditBioBorder' value={bio} onChange={e=> setBio(e.target.value)} type='bio' as='textarea' rows={5}/>
                         </Form.Group>
                         
 
                         <Form.Group className='pEditContactForm'>
                             <Form.Label>Contact Info</Form.Label>
-                            <Form.Control value={contact} onChange={e=> setContact(e.target.value)} type='contact' as='textarea'/>
+                            <Form.Control className='pEditContactBorder' value={contact} onChange={e=> setContact(e.target.value)} type='contact' as='textarea' rows={5}/>
                         </Form.Group>
 
+                        <Button onClick={consoleLogSkills}className='pUpdateProfileButton'>Save Changes</Button>
                     </Form>
+                </Col>
+
+                <Col className='pEditSkillsCol'>
+                    <div className='pEditPersonSkills'>
+                        Skills
+                        <div className='pEditPersonSelect'>
+                            <Select
+                                className='pSkillSelectBorder'
+                                options={personSkills}
+                                placeholder='Select skills here'
+                                isMulti
+                                autoFocus
+                                onChange={setSkills}
+                                />
+                        </div>        
+                    </div>
+                
                 </Col>
             </Row>
         </Container>
