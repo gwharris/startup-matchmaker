c47a599b3cdec1be595bb8b9d408c3c86e5416f7
dleguisamo <david.leguisamo@nyu.edu>
2021-12-07 13:41:35 -0500

    Styling changes to homepage. Also set up fetching data from user

--- frontend/src/components/PersonHomepage.js
+++ frontend/src/components/PersonHomepage.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 // import { StartupItem, Navbar } from 'Navbar.js'
 import AppNavbar from './AppNavbar'
+import {Button, Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import './styles/personHomepage.css';
 
 const PersonHomepage = () => {
+
+    const [name, setName ] = useState(null);
+
+    const [error, setError ] = useState(null);
+    const [loading, setLoading] = useState(null);
+
+    useEffect(() => {
+        // https://api.github.com/users/dleguisamo
+        // /api/getUserProfile
+
+        axios.get("/api/getUserProfile")
+            .then((response) => {
+                console.log(response.data);
+                setName(response.data.name);
+            })
+            .catch((error) => {
+                console.error("Could not fetch data: ", error);
+                setError(error)
+            })
+            .finally(() => {
+                setLoading(false)
+            });
+    }, []);
+
+    if (loading) return "Loading data...";
+    if (error) return "An error!"
+
     return (
-        <AppNavbar/>
+        
+        <Container className='phContainer'>
+            
+            <AppNavbar/>
+            <Row className='phWelcomeRow'>
+                <Col className='phWelcomeCol'>
+                    <div className='m1'>Welcome, {name}</div>
+                    <div className='m2'>Here are your matches based on your skills!</div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 
