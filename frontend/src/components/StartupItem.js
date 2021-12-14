740ec77d9f7c223f816a7757aa1bada1cf9c8eb9
Brian Hsieh <58496626+bhsieh74@users.noreply.github.com>
2021-12-13 21:05:56 -0500

    uploaded correct file

--- frontend/src/components/StartupItem.js
+++ frontend/src/components/StartupItem.js
@@ -1,15 +1,54 @@
 import React from 'react'
-import {Button, Container, Row, Col, Spinner } from 'react-bootstrap';
+import {Button, Container, Row, Col, Spinner} from 'react-bootstrap';
 import './styles/StartupItem.css'
+import mascot from './styles/mascot.png'
 //on mount, fetch matches? 
+import Modal from 'react-bootstrap/Modal'
+
+
+
 const StartupItem = (props) => {
+    const [modalShow, setModalShow] = React.useState(false);
+
     return (
-        <Container className="startupItemContainer">
+        <Container onClick={() => setModalShow(true)} className="startupItemContainer">
             {/* <div style={{ backgroundImage: `url(${image})` }}> </div> */}
-            <h1>{props.name}</h1>
+            <h1 className="startupName">{props.name}</h1>
             <p>{props.bio}</p>
+            <img src={mascot} className='img-fluid'/>
+
+            <StartupModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
         </Container>
     )
+}
+function StartupModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        
+        <Modal.Body>
+          <h2>Company Name</h2>
+          <h4>Company Bio</h4>
+          <br></br>
+          <h5>Desired Skills</h5>
+            <dl>
+            <dt>Skill 1</dt>
+            <dt>Skill 2</dt>
+            </dl>
+        </Modal.Body>
+        <Modal.Footer>
+            <p>Company Contact Information</p>
+          <Button onClick={props.onHide}>Press Esc to Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
 }
 
 export default StartupItem
